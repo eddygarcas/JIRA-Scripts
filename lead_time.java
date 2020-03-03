33fe41815c38df5e862c5374d9a96b2426005082
Eduard Garcia <edugarcas@gmail.com>
2020-03-03 14:30:27 +0100

    Update lead_time.java

--- lead_time.java
+++ lead_time.java
@@ -8,22 +8,35 @@ import com.atlassian.jira.issue.Issue
 import java.text.SimpleDateFormat;
 import java.text.DateFormat
 import org.apache.log4j.Logger
+import com.atlassian.jira.issue.status.category.StatusCategory
 
 def log = Logger.getLogger("com.onresolve.scriptrunner.runner.ScriptRunnerImpl")
-def closedStatus = ["done","closed","rejected","resolved","drop","ok"]
+def stillWip = (issue.getStatus().getStatusCategory().getKey() == StatusCategory.IN_PROGRESS) ? true : false
 
-def changeHistoryManager = ComponentAccessor.getChangeHistoryManager()
-def initialStatus = changeHistoryManager.getAllChangeItems(issue).get(0)
-def changeItems = changeHistoryManager.getChangeItemsForField(issue,"status")
+String holidays = "2019-12-06,2019-12-08,2019-12-24,2019-12-25,2019-12-26,2019-12-31,"+
+                  "2020-01-01,2020-04-19,2020-04-22,2020-05-01,2020-06-24,2020-08-15," +
+    			  "2020-09-11,2020-09-24,2020-11-01,2020-12-06,2020-12-08,2020-12-24," +
+    			  "2020-12-25,2020-12-25,2020-12-26,2020-12-31,"+
+    			  "2021-01-01"
+
+
+def workflow = ComponentAccessor.getWorkflowManager().getWorkflow(issue)
+def closedStatus = []
+workflow.getLinkedStatusObjects().each{ status ->
+    if (status.getStatusCategory().getKey() == StatusCategory.COMPLETE) {
+        closedStatus << status.getName().toLowerCase()
+    }
+}
+
 
 def inProgressTime=0
 def doneTime=0
 def days = 0
-def stillWip = false
 
-String holidays = "2018-12-06,2018-12-08,2018-12-24,2018-12-25,2018-12-26,2018-12-31,"+
-                  "2019-01-01,2019-04-19,2019-04-22,2019-05-01,2019-06-24,2019-08-15,2019-09-11,2019-09-24,2019-11-01,2019-12-06,2019-12-08,2019-12-24,2019-12-25,2019-12-25,2019-12-26,2019-12-31,"+
-    			  "2020-01-01"
+
+def changeHistoryManager = ComponentAccessor.getChangeHistoryManager()
+def initialStatus = changeHistoryManager.getAllChangeItems(issue).get(0)
+def changeItems = changeHistoryManager.getChangeItemsForField(issue,"status")
 
 inProgressTime = initialStatus.getCreated().getTime()
 changeItems.each{
@@ -35,12 +48,9 @@ changeItems.each{
 if (inProgressTime!=0){
     if(doneTime==0){
     	doneTime=System.currentTimeMillis()
-        stillWip = true
 	}
     Calendar startCal = Calendar.getInstance()
     startCal.setTime(new Date((long)inProgressTime))
-    
-    
     
     Calendar endCal = Calendar.getInstance()
     endCal.setTime(new Date((long)doneTime))
@@ -58,4 +68,4 @@ if (inProgressTime!=0){
 }
 
 //return working days
-return "<p style='color:" + (stillWip ? "green" : "black")  + "'><strong>" + (days as long ?: 0L) + "</strong> day(s) Open</p>"
+return "<p style='color:" + (stillWip ? "green" : "blue-gray")  + "'><strong>" + (days as long ?: 0L) + "</strong>d to " + issue.getStatus().getStatusCategory().getTranslatedName() +"</p>"
